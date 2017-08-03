54d73c2db4fb927eeb4571edd7bad264c61e3c82
Joe Kausits <JoeKausits@gmail.com>
2017-08-03 08:38:46 -0400

    Set up start script and heroku port

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 
 hbs.registerPartials(__dirname + '/views/partials')
 
+// HELPERS
 hbs.registerHelper('getCurrentYear', ()=>{
   return new Date().getFullYear();
 })
-
 hbs.registerHelper('screamIt', (text)=>{
   return text.toUpperCase();
 })
 
 app.set('view engine', 'hbs');
 
-// Configuring middleware
+// MIDDLEWARE
 app.use((req, res, next)=>{
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
@@ -25,18 +28,18 @@ app.use((req, res, next)=>{
   next();
 });
 
-app.use((req, res, next)=>{
-  res.render('maintenance.hbs')
-});
+// app.use((req, res, next)=>{
+//   res.render('maintenance.hbs')
+// });
 app.use(express.static(__dirname + '/public'));
 
+// ROUTES
 app.get('/', (req, res)=>{
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to the jungle'
   });
 });
-
 
 app.get('/about', (req, res)=>{
   res.render('about.hbs', {
@@ -50,7 +53,8 @@ app.get('/bad', (req, res)=>{
   })
 });
 
-app.listen(3000,()=>{
-  console.log("Server is up on port 3000");
+// Starting server
+app.listen(port,()=>{
+  console.log(`Server is up on port ${port}`);
 }
 );
